4faed2aed194f045a99ff632e2c7b21cffc71462
Eric-Jonathan <ericjonathanliem@gmail.com>
2024-05-25 19:42:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let notif = document.getElementById("popup");
+
+function openNotif(){
+    notif.classList.add("openNotifikasi");
+}
+function closeNotif(){
+    notif.classList.remove("openNotifikasi");
+}
